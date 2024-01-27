200f66c5300444ad3b041db951a2c4413921952c
anish-singh-rawat <anishsinghrawat1>
2024-01-27 15:53:42 +0530

    update code

--- model/User.ts
+++ model/User.ts
@@ -1,37 +1,49 @@
-import mongoose,  { Schema } from "mongoose";
-const userSchema = new Schema({
-    firstName : {
-        require : [true],
-        type : Schema.Types.String,
-    },  
-    lastName : {
-        require : [true],
-        type : Schema.Types.String,
-    },
-    email : {
-        require : [true],
-        type : Schema.Types.String,
-    },
-    fatherName : {
-        require : [true],
-        type : Schema.Types.String,
-    },
-    motherName : {
-        require : [true],
-        type : Schema.Types.String,
-    },
-    address : {
-        require : [true],
-        type : Schema.Types.String,
-    }, 
-    pincode : {
-        require : [true],
-        type : Schema.Types.String,
-    },
-    country : {
-        require : [true],
-        type : Schema.Types.String,
-      }
-})
+import mongoose, { Schema, Document } from "mongoose";
 
-export const User : any =  mongoose.models.User ||  mongoose.model("User", userSchema);
+interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  fatherName: string;
+  motherName: string;
+  address: string;
+  pincode: string;
+  country: string;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+  firstName: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  lastName: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  email: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  fatherName: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  motherName: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  address: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  pincode: {
+    required: true,
+    type: Schema.Types.String,
+  },
+  country: {
+    required: true,
+    type: Schema.Types.String,
+  },
+});
+
+export const User = mongoose.models.User as mongoose.Model<IUser> || mongoose.model<IUser>("User", userSchema);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,130 +10,110 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useRouter } from 'next/navigation';
 
-
-export default function page() {
+export default function Page() {
   const router = useRouter();
-  const [userInfo, setUserInfo] = useState<any>({
-      firstName : "",
-      lastName : "",
-      email : "",
-      fatherName : "",
-      motherName : "",
-      address : "",
-      pincode : '',
-      country : "",
+  const [userInfo, setUserInfo] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    fatherName: "",
+    motherName: "",
+    address: "",
+    pincode: "",
+    country: "",
   });
 
-  const handleSubmit = async (e : any)=>{
+  const inputFields = [
+    { label: "First Name", name: "firstName" },
+    { label: "Last Name", name: "lastName" },
+    { label: "Email", name: "email" },
+    { label: "Father Name", name: "fatherName" },
+    { label: "Mother Name", name: "motherName" },
+    { label: "Address", name: "address" },
+    { label: "PIN code", name: "pincode", type: "number" },
+  ];
+
+  const handleSubmit = async (e : any) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/registration", userInfo)
-      if(res.status === 201){
-        toast.success("User Registered Successfully")
-        router.push('/dashboard')
+      const res = await axios.post("/api/registration", userInfo);
+      if (res.status === 201) {
+        toast.success("User Registered Successfully");
+        router.push('/dashboard');
         setUserInfo({
-            firstName : "",
-            lastName : "",
-            email : "",
-            fatherName : "",
-            motherName : "",
-            address : "",
-            pincode : '',
-            country : "",
-        })
+          firstName: "",
+          lastName: "",
+          email: "",
+          fatherName: "",
+          motherName: "",
+          address: "",
+          pincode: "",
+          country: "",
+        });
       }
     } catch (error) {
       console.log(error);
     }
-  }
-
+  };
 
   return (
     <>
-    <ToastContainer/>
-      <div className="container-fluid containe-for-sub-box ">
+      <ToastContainer />
+      <div className="container-fluid containe-for-sub-box">
         <div className="row">
           <div className="col">
             <div className="parent-form-div">
               <div className="sub-form-data">
-                <div className="form-top-heading mt-2">SinUp</div>
-                <form className='mt-2' onSubmit={handleSubmit} >
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'> First Name </label>
-                    <input type="text" name='username'
-                      className='user-name-input'
-                      value={userInfo.firstName}
-                      onChange={(e) => setUserInfo({ ...userInfo, firstName : e.target.value })}  />
-                  </div>
-
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'> Last Name </label>
-                    <input type="text" name='username'
-                      className='user-name-input ' 
-                      value={userInfo.lastName}
-                      onChange={(e) => setUserInfo({ ...userInfo, lastName : e.target.value })}  />
-                  </div>
-
-                  <div className="user-password-feild">
-                    <label htmlFor="" className='mt-2 user-password-label-feild'>Email </label>
-                    <input type="text" name='email'
-                      className='user-password-input'
-                      value={userInfo.email}
-                      onChange={(e) => setUserInfo({ ...userInfo, email : e.target.value })} />
-                  </div>
-
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'> Father Name </label>
-                    <input type="text" name='username'
-                      className='user-name-input '
-                      value={userInfo.fatherName}
-                      onChange={(e) => setUserInfo({ ...userInfo, fatherName : e.target.value })}  />
-                  </div>
-
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'> Motehr Name </label>
-                    <input type="text" name='username'
-                      className='user-name-input '
-                      value={userInfo.motherName}
-                      onChange={(e) => setUserInfo({ ...userInfo, motherName : e.target.value })}  />
-                  </div>
-
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'>Address</label>
-                    <input type="text" name='username'
-                      className='user-name-input '
-                      value={userInfo.address}
-                      onChange={(e) => setUserInfo({ ...userInfo, address : e.target.value })}  />
-                  </div>
-
-                  <div className="user-name-feild-div">
-                    <label htmlFor="" className='user-name-label-feild mt-2'>PIN code </label>
-                    <input type="number" name='username'
-                      className='user-name-input ' 
-                      value={userInfo.pincode}
-                      onChange={(e) => setUserInfo({ ...userInfo, pincode : e.target.value })}   />
-                  </div>
+                <div className="form-top-heading mt-2">SignUp</div>
+                <form className="mt-2" onSubmit={handleSubmit}>
+                  {inputFields.map((field) => (
+                    <div key={field.name} className="user-name-feild-div">
+                      <label htmlFor="" className="user-name-label-feild mt-2">
+                        {field.label}
+                      </label>
+                      <input
+                        type={field.type || "text"}
+                        name={field.name}
+                        className="user-name-input"
+                        value={userInfo[field.name]}
+                        onChange={(e) =>
+                          setUserInfo({
+                            ...userInfo,
+                            [field.name]: e.target.value,
+                          })
+                        }
+                      />
+                    </div>
+                  ))}
                   <br />
-                   <Box sx={{ minWidth: 10, marginRight : '30px' }}>
-                     <FormControl fullWidth>
-                       <InputLabel id="demo-simple-select-label">Country</InputLabel>
-                       <Select
-                         labelId="demo-simple-select-label"
-                         id="demo-simple-select"
-                         label="age"
-                         value={userInfo.country}
-                         onChange={(e) => setUserInfo({ ...userInfo, country : e.target.value })}>
-                         <MenuItem value={'India'}>India</MenuItem>
-                         <MenuItem value={'Nepal'}>Nepal</MenuItem>
-                         <MenuItem value={"Bangla Desh"}> Bangla desh </MenuItem>
-                       </Select>
-                     </FormControl>
-                  </Box> 
+                  <Box sx={{ minWidth: 10, marginRight: '30px' }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">Country</InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="age"
+                        value={userInfo.country}
+                        onChange={(e) =>
+                          setUserInfo({
+                            ...userInfo,
+                            country: e.target.value,
+                          })
+                        }
+                      >
+                        <MenuItem value={'India'}>India</MenuItem>
+                        <MenuItem value={'Nepal'}>Nepal</MenuItem>
+                        <MenuItem value={"Bangla Desh"}>Bangla desh</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
                   <br />
                   <center>
-                    <button type='submit'
+                    <button
+                      type="submit"
                       className="btn text-white mb-2"
-                      style={{ backgroundColor: '#2c3e50' }}>
+                      style={{ backgroundColor: '#2c3e50' }}
+                    >
                       Submit
                     </button>
                   </center>
