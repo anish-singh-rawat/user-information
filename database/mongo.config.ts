d5188647b96dc70ed23e80de103bcb8a631807c0
anish-singh-rawat <anishsinghrawat1>
2024-01-27 15:22:21 +0530

    update code

--- database/mongo.config.ts
+++ database/mongo.config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connect() {
     try {
-        await mongoose.connect(process.env.MONGODB_URL!, {
+        await mongoose.connect(`mongodb+srv://${process.env.DBNAME}:${process.env.PASS}@cluster0.5wh3n75.mongodb.net/`, {
             tls: true,
         });
         console.log('Successfully connected');
