4cb94b349028c6b91aaa16e3b1fbf85e0e1a43e5
anish-singh-rawat <anishsinghrawat1>
2024-01-27 16:57:28 +0530

    update code

--- src/app/api/getusers/route.ts
+++ src/app/api/getusers/route.ts
@@ -1,9 +1,7 @@
 import {  NextResponse } from "next/server";
 import { User } from "../../../../model/User";
-import { connect } from "../../../../database/mongo.config";
 
 export async function GET (){
-    connect()
     const topic = await User.find();
     return NextResponse.json({topic})
 }
