2dbc61b42a8fd3f00e51bf7fc86ec46d5042c0cc
anish-singh-rawat <anishsinghrawat1>
2024-01-27 17:09:51 +0530

    update code

--- src/components/UserData/UserData.tsx
+++ src/components/UserData/UserData.tsx
@@ -6,24 +6,15 @@ import UsersData from '../UsersData'
 const UserData = () => {
   const [userData, setUserData] = useState<any>([])
   useEffect(() => {
-    const test = getServerSideProps();
-    test.then((res) => {
-      setUserData(res.userData)
-    })
+    const allUser =async()=>{
+      const res =  await axios.get('/api/getusers')
+      setUserData(res.data.topic)      
+    }
+    allUser();
   }, [])
 
   return (
     <UsersData userData={userData}/>
   )
 }
-
-const getServerSideProps = async () => {
-  const res = await axios.get('/api/getusers')
-  const userData = res.data.topic
-  return { 
-    userData 
-  }
-}
-
-
 export default UserData
